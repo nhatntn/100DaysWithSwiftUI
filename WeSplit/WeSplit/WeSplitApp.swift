810fd59d750102efe6512ce38f80d6b1d8842f08
Nhat Nguyen <nhat.nguyen@elsanow.io>
2022-06-15 23:06:34 +0700

    feat(wesplit): init project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by nhatnt on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
