c3396da4d963db5a74141be3e20fb3118dfcc6ec
Nhat Nguyen <nhat.nguyen@elsanow.io>
2022-06-21 11:52:45 +0700

    feat(gtf): initialize project with vstack, hstack, zstack

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by nhatnt on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
